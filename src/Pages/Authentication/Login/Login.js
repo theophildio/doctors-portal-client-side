2a940a13e31289f240a467493ef39498c12be5d8
theophildio <dtheo.sangma@gmail.com>
2022-05-15 00:26:11 +0600

    Google authentication added

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
+import auth from "../../../firebase.init";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 const Login = () => {
-  return (
-    <div>
-      <h1>this is login</h1>
-    </div>
-  );
+
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  
+  if (user) {
+    console.log(user);
+  }
+
+	return (
+		<div className="flex h-screen justify-center items-center">
+			<div className="card w-96 bg-base-100 shadow-xl">
+				<div className="card-body">
+					<h2 className="text-2xl font-bold text-center">Login</h2>
+					<div className="divider">OR</div>
+          <button onClick={() => signInWithGoogle()} className="btn btn-active">CONTINUE WITH GOOGLE</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
